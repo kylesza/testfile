df1edf9853d5779da814fa7c439c2aa4f5510395
kylesza <kyle.kulesza@gmail.com>
2016-09-20 22:24:23 -0400

    first commit, not working, erroring out

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'resumes/index'
+
+  get 'resumes/new'
+
+  get 'resumes/create'
+
+  get 'resumes/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+CarrierWaveExample::Application.routes.draw do
+   resources :resumes, only: [:index, :new, :create, :destroy]
+   root "resumes#index"
+end
